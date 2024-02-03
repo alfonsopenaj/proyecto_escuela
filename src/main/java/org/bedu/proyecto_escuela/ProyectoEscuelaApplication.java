36f81bca4ebf993751babf0b780694f844b0f75b
Alejodev <honki.ponki.dance@gmail.com>
2024-02-03 09:29:47 -0600

    Agregando html maestro y materia

--- src/main/java/org/bedu/proyecto_escuela/ProyectoEscuelaApplication.java
+++ src/main/java/org/bedu/proyecto_escuela/ProyectoEscuelaApplication.java
@@ -2,6 +2,11 @@ package org.bedu.proyecto_escuela;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.standard.StandardDialect;
+
 
 @SpringBootApplication
 public class ProyectoEscuelaApplication {
@@ -9,4 +14,5 @@ public class ProyectoEscuelaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoEscuelaApplication.class, args);
 	}
+
 }
